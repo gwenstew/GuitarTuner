2210b346d723637ac16028c4db4545135262d63d
gwenstew <gwenstew29@oulook.com>
2025-06-19 12:58:52 -0400

    refactoring MainComponent to be a subclass of audio app component; initializing functions to be overriden to implemenet guitar tuner

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -1,13 +1,31 @@
 //will contain logic for gui component
 #include "MainComponent.h"
 
-//==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setAudioChannels(1,0);  //set number of input (1) and output channels (0);
+    setSize (600, 400);     //set size of display window
 }
 
-//==============================================================================
+MainComponent::~MainComponent() {
+    shutdownAudio();
+}
+
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // audio setup here
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    // real-time audio input here
+}
+
+void MainComponent::releaseResources()
+{
+    // cleanup audio buffers/resources
+}
+
 void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
--- src/MainComponent.h
+++ src/MainComponent.h
@@ -1,29 +1,22 @@
 #pragma once
+#include <JuceHeader.h>
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
-// directly. If you need to remain compatible with Projucer-generated builds, and
-// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
-// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent   : public juce::Component
+class MainComponent : public juce::AudioAppComponent
 {
 public:
-    //==============================================================================
-    MainComponent();
 
-    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
